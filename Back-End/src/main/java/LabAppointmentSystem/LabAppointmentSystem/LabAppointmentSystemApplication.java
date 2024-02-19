a074f2429ab941a0a9d750ed7a5f5ec3c702285e
ashenshandeepoffl <ashenshandeepoffl@gmail.com>
2024-02-19 09:45:10 +0530

    Lab Appointment System

---
+++ Back-End/src/main/java/LabAppointmentSystem/LabAppointmentSystem/LabAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package LabAppointmentSystem.LabAppointmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabAppointmentSystemApplication.class, args);
+	}
+
+}
