e68d2f92fb4c671ca7dd0c81d07c245778946cdf
ashenshandeepoffl <ashenshandeepoffl@gmail.com>
2024-02-20 07:48:40 +0530

    Updated the navigation bar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Importing Routes instead of Switch
+import Navbar from './Navbar';
+import Home from './Home'; // Import your Home component
+import About from './About'; // Import your About component
+import Contact from './Contact'; // Import your Contact component
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes> {/* Change from Switch to Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes> {/* Change from Switch to Routes */}
+    </Router>
   );
 }
 
